3927a44932ac5cffdd52711f8943a279747e3e0c
julien-alr <julienalaria@gmail.com>
2025-02-03 12:40:06 +0100

    update

--- jour01/job01/job01.php
+++ jour01/job01/job01.php
@@ -1,1 +1,19 @@
+<?php
+
+class Operation {
+
+    public $nombre1;
+    public $nombre2;
+
+    public function __construct($nombre1 = 7, $nombre2 = 8) {
+        $this->nombre1 = $nombre1;
+        $this->nombre2 = $nombre2;
+    }
+}
+
+$operation = new Operation();
+
+echo "Nombre 1 : " . $operation->nombre1 . "<br>";
+echo "Nombre 2 : " . $operation->nombre2 . "<br>";
+?>
 
