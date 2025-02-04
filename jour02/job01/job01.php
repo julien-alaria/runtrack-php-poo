c2908bc44907b9a9a9f876022fbe16093e420c20
julien-alr <julienalaria@gmail.com>
2025-02-04 11:02:10 +0100

    first commit

--- jour02/job01/job01.php
+++ jour02/job01/job01.php
@@ -1,1 +1,52 @@
+<?php
 
+class Rectangle{
+
+    private $longueur;
+    private $largeur;
+
+    public function __construct($longueur, $largeur){
+        $this->longueur = $longueur;
+        $this->largeur = $largeur;
+
+    }
+
+    public function setLongueur($newLongueur){
+        $this->longueur = $newLongueur;
+        
+    }
+
+    public function setLargeur($newLargeur){
+        $this->largeur = $newLargeur;
+        
+    }
+
+    public function getLongueur(){
+        return $this->longueur."<br>";
+        
+    }
+
+    public function getLargeur(){
+        return $this->largeur."<br>";
+        
+    }
+}
+
+$rec = New Rectangle(10, 5);
+
+echo "Longueur initiale: <br>";
+echo $rec->getLongueur();
+echo "Largeur initiale: <br>";
+echo $rec->getLargeur();
+
+$rec->setLongueur(30);
+$rec->setLargeur(50);
+
+echo "Nouvelle Longueur: <br>";
+echo $rec->getLongueur();
+echo "Nouvelle largeur: <br>";
+echo $rec->getLargeur();
+
+
+
+?>
